30639042aaeb968efd29886e95a24171ac603013
UrbYy <98297696+Kenjtana@users.noreply.github.com>
2022-03-28 19:26:28 +0700

    Add files via upload

---
+++ TestSiswa.java
@@ -0,0 +1,10 @@
+public class TestSiswa {
+    public static void main(String[] args) {
+        encapsiswa siswa = new encapsiswa();
+        siswa.setName("Agus");
+        siswa.setAge(20);
+        siswa.setAddress("Malang");
+
+        System.out.println("Nama "+siswa.getName()+" alamat "+siswa.getAddress()+" berumur "+siswa.getAge()+" tahun");
+    }
+}
---
+++ encapsiswa.java
@@ -0,0 +1,31 @@
+public class encapsiswa{
+    private String name;
+    private String address;
+    private int age;
+
+    //  method get
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    // method set
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+}
